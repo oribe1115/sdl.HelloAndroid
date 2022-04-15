dd3303419ccfa65f2e80aa77184bce834db1945e
oribe(oribe1115) <ayaka.k1n05h17a@gmail.com>
2022-04-15 18:16:29 +0900

    挨拶するように書き換えた

--- app/src/main/java/jp/ac/titech/itpro/sdl/helloandroid/MainActivity.kt
+++ app/src/main/java/jp/ac/titech/itpro/sdl/helloandroid/MainActivity.kt
@@ -1,11 +1,26 @@
 package jp.ac.titech.itpro.sdl.helloandroid
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var outputView: TextView
+    private lateinit var inputName: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        outputView = findViewById(R.id.output_view)
+        inputName = findViewById(R.id.input_name)
+    }
+
+    fun greet(v: View) {
+        val name = inputName.text.toString().trim()
+        if (name.isNotEmpty()) {
+            outputView.text = "Hello ${name}!\nNice to see you!"
+        }
     }
 }
